dc8ce2f390debdc7af493a0107766b2c785d8ca6
Kristen <kawilliams3@gmail.com>
2015-03-23 18:48:46 -0500

    dashapptnotesfixedup

--- js/Scheduling.js
+++ js/Scheduling.js
@@ -8,54 +8,109 @@
             console.log("initialized");
 
             this.model = new Parse.Session();
-            // this.object = new Parse.Session();
             this.homeView = new Parse.HomeView({
                 model: this.object
             });
+
+            this.appointments = new Parse.AppointmentCollection();
             this.apptRequestView = new Parse.ApptRequestView({
-                collection: this.collection
+                collection: this.appointments
             });
-            this.scheduleView = new Parse.ScheduleView();
-            this.notesView = new Parse.NotesView();
-            this.loginView = new Parse.LoginView();
             this.patientHomeView = new Parse.PatientHomeView({
+                collection: this.appointments
                 // collection: this.collection
                 // collection: Parse.AppointmentCollection
             });
+            this.notes = new Parse.NotesCollection();
+            // Parse.NotesCollection has model: Parse.Appointment
+            this.patientNotesView = new Parse.PatientNotesView({
+                collection: this.notes
+            });
+
+            this.scheduleView = new Parse.ScheduleView();
+            this.notesView = new Parse.NotesView();
+            this.loginView = new Parse.LoginView();
+
             Parse.history.start();
         },
         routes: {
-            "apptrequest": "apptrequest",
+            "apptrequest": "loadApptrequest",
             "schedule": "schedule",
             "notes": "notes",
+            "notes/:id": "loadVistNotes",
             "login": "login",
+            //"dashboard/:patientId": "loadPatientDashboard"
             "dashboard": "patienthome",
             "*default": "home"
         },
-        apptrequest: function() {
+        loadApptrequest: function() {
             // var query = new Parse.Query(Parse.Appointment);
             // query.equalTo("user", this.user);
+            // Parse.AppointmentCollection.query = query;
+            // Parse.AppointmentCollection.fetch();
             // this.collection.query = query;
             // this.collection.fetch();
-            this.apptRequestView.render();
+            var self = this;
+            // self = router
+            this.appointments.fetch().then(function(collectionResult) {
+                // the result should be a Parse.Collection, hopefully
+                self.apptRequestView.collection = collectionResult;
+                self.apptRequestView.render();
+                // self because it needs to be a part of this model, new function
+            });
         },
+        // loadTodaysAppointents: function(){
+
+        //     var today = new Date();
+
+        //     var pQuery = new Parse.Query(Parse.Appointment)
+        //     pQuery.equalTo('date', today)
+
+        //     pQuery.find().then(function(result){
+        //         var todaysAppointments = result
+
+        //         self.apptRequestView.collection = todaysAppointments;
+        //         self.apptRequestView.render()
+
+
+        //     });
+        // },
         schedule: function() {
             this.scheduleView.render();
         },
         notes: function() {
             this.notesView.render();
         },
+        loadVistNotes: function() {
+            var self = this;
+            // self = router
+            this.appointments.fetch().then(function(collectionResult) {
+                // the result should be a Parse.Collection, hopefully
+                self.patientNotesView.collection = collectionResult;
+                self.patientNotesView.render();
+                // self because it needs to be a part of this model, new function
+            });
+            // this.patientNotesView.render();
+        },
         login: function() {
             this.loginView.render();
         },
         patienthome: function() {
-            var query = new Parse.Query(Parse.Appointment);
-            query.equalTo("user", this.user);
+            // var query = new Parse.Query(Parse.Appointment);
+            // query.equalTo("user", this.user);
             // Parse.AppointmentCollection.query = query;
             // Parse.AppointmentCollection.fetch();
             // this.collection.query = query;
             // this.collection.fetch();
-            this.patientHomeView.render();
+            var self = this;
+            // self = router
+            this.appointments.fetch().then(function(collectionResult) {
+                // the result should be a Parse.Collection, hopefully
+                self.patientHomeView.collection = collectionResult;
+                self.patientHomeView.render();
+                // self because it needs to be a part of this model, new function
+            });
+            // this.patientHomeView.render();
         },
         home: function() {
             this.homeView.render();
@@ -74,9 +129,11 @@
         defaults: {
             // date: new Date("Jan 1 1970"),
             date: new Date(1970, 0, 0),
+            dateReadable: "",
             description: "",
             id: "",
             notes: "",
+            doctorNotes: "",
             user: null,
             occurred: false
         },
@@ -87,8 +144,18 @@
         }
     })
 
+    // Parse.Note = Parse.Object.extend({
+    //     className: "note",
+
+    // })
+
     Parse.AppointmentCollection = Parse.Collection.extend({
         model: Parse.Appointment
+    })
+
+    Parse.NotesCollection = Parse.Collection.extend({
+        model: Parse.Appointment
+        // model: Parse.Note
     })
 
     Parse.ApptRequestView = Parse.TemplateView.extend({
@@ -96,13 +163,14 @@
         view: "bootstrap-apptrequest",
         events: {
             // submit the form sendrequest from bootstrap-apptrequest.html
-            // change add the textarea info of that form to appointments in the PatientHomeView
-            "submit .sendrequest": "showappt"
+            // activity on the DOM
+            "submit .sendrequest": "saveappt"
         },
-        showappt: function(event) {
+        saveappt: function(event) {
             event.preventDefault();
             // console.log(event);
-
+            var loggedInUser = Parse.User.current();
+            var acl = new Parse.ACL(loggedInUser);
             // console.log(this.el.querySelector(".sendrequest input[name='apptdate']").value);
             // line above: HTML gives date as a string ie 2015-03-25 to JS
             var dateString = this.el.querySelector(".sendrequest input[name='apptdate']").value;
@@ -126,12 +194,31 @@
             });
 
             // console.log(dateArrayNumbers);
+            var monthsArray = [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+            ]
 
-            var loggedInUser = Parse.User.current();
-            var acl = new Parse.ACL(loggedInUser);
+            var jsDate = new Date(dateArrayNumbers[0], dateArrayNumbers[1], dateArrayNumbers[2]);
+
+            var theMonth = jsDate.getMonth();
+            var dateReadableString = monthsArray[theMonth] + " " + jsDate.getDate() + " " + jsDate.getFullYear();
+
+
             var appointment = new Parse.Appointment({
-                date: new Date(dateArrayNumbers[0], dateArrayNumbers[1], dateArrayNumbers[2]),
-                description: this.el.querySelector(".sendrequest textarea[name='concerns']").value
+                date: jsDate,
+                description: this.el.querySelector(".sendrequest textarea[name='concerns']").value,
+                dateReadable: dateReadableString
             });
 
 
@@ -139,29 +226,11 @@
             appointment.setACL(acl);
             appointment.save().then(function(data) {
                 console.log(data); //same as console.log(appointment) above
+                // id =appointment.id
                 console.log('save successful');
                 alert("Appointment Request Submitted");
             });
 
-            var appt = new Parse.Appointment({
-                id: id
-            });
-            appt.fetch().then(function() {
-                console.log(appt);
-            });
-
-            // var appt = new Parse.Appointment({
-            //     id: id
-            // });
-            // appt.fetch().then(function() {
-            //     console.log(appt);
-            // });
-
-
-            var appointments = new Parse.AppointmentCollection();
-            appointments.fetch().then(function() {
-                console.log(appointments);
-            });
         }
     })
 
@@ -173,6 +242,35 @@
     Parse.NotesView = Parse.TemplateView.extend({
         el: ".wrapper",
         view: "notes"
+    })
+
+    Parse.PatientNotesView = Parse.TemplateView.extend({
+        el: ".wrapper",
+        view: "bootstrap-notes",
+        events: {
+            "submit .savepersonalnotes": "savenotes"
+        },
+        savenotes: function(event) {
+            event.preventDefault();
+            var loggedInUser = Parse.User.current();
+            var acl = new Parse.ACL(loggedInUser);
+            var note = new Parse.Appointment({
+
+                notes: this.el.querySelector(".savepersonalnotes textarea[name='visitnotes']").value
+
+            });
+
+
+            console.log(note); //same as console.log(data) below
+            note.setACL(acl);
+            note.save().then(function(data) {
+                console.log(data); //same as console.log(appointment) above
+                // id =appointment.id
+                console.log('save successful');
+                alert("Personal Note Saved");
+            });
+
+        }
     })
 
     Parse.LoginView = Parse.TemplateView.extend({
@@ -190,7 +288,7 @@
             }
             var result = Parse.User.logIn(data.username, data.password); //documentation logIn vs login
             result.then(function() {
-                window.location.hash = "#dashboard"
+                window.location.hash = "#dashboard"  //   +"/"+this.model.get('id')
             })
         },
         register: function(event) {
@@ -249,5 +347,6 @@
     //         console.log(appointments)
     //     })
     // }
+
 
 })(typeof module === "object" ? module.exports : window)
